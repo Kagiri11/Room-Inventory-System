9b9ef6619752b133ecafc701f185965e09cfdd8c
Kagiri11 <charlesmaish423@gmail.com>
2021-09-14 01:44:19 +0300

    feat: added sell screen

--- app/src/main/java/com/example/myshop/ui/HomeFragment.kt
+++ app/src/main/java/com/example/myshop/ui/HomeFragment.kt
@@ -5,14 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.example.myshop.R
+import com.example.myshop.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    lateinit var binding: FragmentHomeBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_home, container, false)
+        binding.apply {
+            upperLayout.setOnClickListener {
+                Toast.makeText(requireContext(),"You have pressed the upperLayout",Toast.LENGTH_LONG).show()
+            }
+            lowerLayout.setOnClickListener {
+                findNavController().navigate(R.id.action_homeFragment_to_sellFragment)
+            }
+        }
+        return binding.root
     }
 }
