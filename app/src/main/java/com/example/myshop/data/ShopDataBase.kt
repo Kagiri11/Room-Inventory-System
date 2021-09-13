3c355747043d68bc14914ee597a870fc1262c7cd
Kagiri11 <charlesmaish423@gmail.com>
2021-09-13 16:48:30 +0300

    removed stock class just to rid off a blocker

--- app/src/main/java/com/example/myshop/data/ShopDataBase.kt
+++ app/src/main/java/com/example/myshop/data/ShopDataBase.kt
@@ -3,16 +3,14 @@ package com.example.myshop.data
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.myshop.model.Item
-import com.example.myshop.model.Stock
 
-@Database(entities = [Item::class,Stock::class],version = 1)
+@Database(entities = [Item::class],version = 1,exportSchema = false)
 abstract class ShopDataBase : RoomDatabase(){
 
     abstract val shopDBDao : ShopDatabaseDao
 
     companion object{
         private var INSTANCE : ShopDataBase?=null
-        val DATABASE_NAME = "shop_db"
+        const val DATABASE_NAME = "shop_db"
     }
-
 }
--- app/src/main/java/com/example/myshop/data/ShopDatabaseDao.kt
+++ app/src/main/java/com/example/myshop/data/ShopDatabaseDao.kt
@@ -2,19 +2,9 @@ package com.example.myshop.data
 
 import androidx.room.*
 import com.example.myshop.model.Item
-import com.example.myshop.model.Stock
 
 @Dao
 interface ShopDatabaseDao {
-
-    @Query("SELECT * FROM stock_table ORDER BY stockId DESC")
-    fun getStocks():List<Stock>
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun addStock(stock: Stock)
-
-    @Delete
-    suspend fun deleteStock(stock: Stock)
 
     @Delete
     suspend fun deleteItem(item: Item)
