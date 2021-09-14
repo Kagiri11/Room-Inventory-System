4e55a130d92cec7cc976f63b578c3696197063d5
Kagiri11 <charlesmaish423@gmail.com>
2021-09-14 18:11:12 +0300

    Feat: Added bottom sheet and its behaviour

--- app/src/main/java/com/example/myshop/ui/SellFragment.kt
+++ app/src/main/java/com/example/myshop/ui/SellFragment.kt
@@ -1,19 +1,45 @@
 package com.example.myshop.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.example.myshop.R
+import com.example.myshop.databinding.FragmentSellBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class SellFragment : Fragment() {
-
+    private lateinit var binding: FragmentSellBinding
+    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sell, container, false)
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_sell, container, false)
+
+        BottomSheetBehavior.from(binding.bottomSheetLayout).apply {
+            peekHeight=200
+            this.state=BottomSheetBehavior.STATE_COLLAPSED
+            addBottomSheetCallback(object :
+                BottomSheetBehavior.BottomSheetCallback() {
+                override fun onStateChanged(bottomSheet: View, newState: Int) {
+                    binding.ivState.background=when(newState){
+                        BottomSheetBehavior.STATE_COLLAPSED-> resources.getDrawable(R.drawable.ic_arrow_up,context?.theme)
+                        else -> resources.getDrawable(R.drawable.ic_arrow_down,context?.theme)
+                    }
+                }
+
+                override fun onSlide(bottomSheet: View, slideOffset: Float) {
+
+                }
+            }
+            )
+        }
+
+
+        return binding.root
     }
 }
