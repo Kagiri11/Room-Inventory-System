3d9601a53479f3088d262510583c23f605896a1e
Kagiri11 <charlesmaish423@gmail.com>
2021-09-15 18:17:06 +0300

    updated add cart

--- app/src/main/java/com/example/myshop/ui/sellFragment/SellFragment.kt
+++ app/src/main/java/com/example/myshop/ui/sellFragment/SellFragment.kt
@@ -2,44 +2,77 @@ package com.example.myshop.ui.sellFragment
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.MutableLiveData
 import com.example.myshop.R
 import com.example.myshop.databinding.FragmentSellBinding
+import com.example.myshop.model.Item
 import com.example.myshop.ui.adapters.SellItemAdapter
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class SellFragment : Fragment() {
 
-    private val sellViewModel : SellViewModel by viewModels()
-
+    private val sellViewModel: SellViewModel by viewModels()
+    private val sellAdapter = SellItemAdapter()
     private lateinit var binding: FragmentSellBinding
-    @SuppressLint("UseCompatLoadingForDrawables")
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_sell, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_sell, container, false)
 
+        sellAdapter.setOnItemClickListener {
+            addToCart(it)
+        }
+
+        handleBottomSheet()
+
+
+
+        sellViewModel.sellCart2.observe(viewLifecycleOwner,{ list->
+            binding.tvItemCount.text = list.sumOf { it.sellingPrice }.toString()
+        })
+
+        sellViewModel.itemCount.observe(viewLifecycleOwner, {
+
+        })
+
+        sellViewModel.items.observe(viewLifecycleOwner, {
+            sellAdapter.differ.submitList(it)
+            binding.rvSellItems.adapter = sellAdapter
+        })
+
+        return binding.root
+    }
+
+    private fun addToCart(item: Item) {
+        sellViewModel.addToCart(item)
+    }
+
+    private fun handleBottomSheet() {
         BottomSheetBehavior.from(binding.bottomSheetLayout).apply {
-            peekHeight=200
-            this.state=BottomSheetBehavior.STATE_COLLAPSED
+            peekHeight = 200
+            this.state = BottomSheetBehavior.STATE_COLLAPSED
             addBottomSheetCallback(object :
                 BottomSheetBehavior.BottomSheetCallback() {
+                @SuppressLint("UseCompatLoadingForDrawables")
                 override fun onStateChanged(bottomSheet: View, newState: Int) {
-                    binding.ivState.background=when(newState){
-                        BottomSheetBehavior.STATE_COLLAPSED-> resources.getDrawable(R.drawable.ic_arrow_up,context?.theme)
-                        else -> resources.getDrawable(R.drawable.ic_arrow_down,context?.theme)
+                    binding.ivState.background = when (newState) {
+                        BottomSheetBehavior.STATE_COLLAPSED -> resources.getDrawable(
+                            R.drawable.ic_arrow_up,
+                            context?.theme
+                        )
+                        else -> resources.getDrawable(R.drawable.ic_arrow_down, context?.theme)
                     }
                 }
 
@@ -49,17 +82,5 @@ class SellFragment : Fragment() {
             }
             )
         }
-
-       sellViewModel.itemCount.observe(viewLifecycleOwner,{
-           binding.tvItemCount.text= it.toString()
-       })
-
-        sellViewModel.items.observe(viewLifecycleOwner,{
-            val sellAdapter = SellItemAdapter(it)
-            binding.rvSellItems.adapter = sellAdapter
-        })
-
-
-        return binding.root
     }
 }
--- app/src/main/java/com/example/myshop/ui/sellFragment/SellViewModel.kt
+++ app/src/main/java/com/example/myshop/ui/sellFragment/SellViewModel.kt
@@ -1,9 +1,7 @@
 package com.example.myshop.ui.sellFragment
 
 import androidx.hilt.lifecycle.ViewModelInject
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.liveData
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.example.myshop.model.Item
 import com.example.myshop.repository.ShopRepository
 import kotlinx.coroutines.flow.collect
@@ -21,20 +19,29 @@ class SellViewModel
         emit(shopRepository.getItems())
     }
 
-    val itemOne = Item(20,"Item One",3.5, 5.0)
-    val itemTwo = Item(13,"Item two",3.5, 5.0)
+
+
+    val itemOne = Item(20,"Heart cake",6.5, 10.0)
+    val itemTwo = Item(13,"Kiberiti",2.0, 5.0)
+    val itemThree = Item(19,"Fritos",100.0, 155.0)
+
+    private val cartList = mutableListOf(itemThree)
+    val _sellCart2 = MutableLiveData<List<Item>>()
+    val sellCart2 : LiveData<List<Item>> = _sellCart2
 
     init {
         add(itemOne)
         add(itemTwo)
+        add(itemThree)
+    }
+
+    fun addToCart(item: Item){
+        cartList.add(item)
+        _sellCart2.postValue(cartList.toList())
     }
 
     fun add(item: Item)=viewModelScope.launch {
         shopRepository.addItem(item)
     }
-
-
-
-
 
 }
