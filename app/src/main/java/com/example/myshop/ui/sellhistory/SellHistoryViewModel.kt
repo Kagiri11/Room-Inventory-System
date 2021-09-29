7adcf5785cefd4cce4aa3e48187b9ea68d61c4c7
Kagiri11 <charlesmaish423@gmail.com>
2021-09-30 01:00:28 +0300

    Implemented logic to find daily profit

--- app/src/main/java/com/example/myshop/ui/sellhistory/SellHistoryViewModel.kt
+++ app/src/main/java/com/example/myshop/ui/sellhistory/SellHistoryViewModel.kt
@@ -3,18 +3,30 @@ package com.example.myshop.ui.sellhistory
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
 import com.example.myshop.data.repositories.ShopRepository
 import com.example.myshop.model.SellEntry
+import kotlinx.coroutines.launch
 
 class SellHistoryViewModel @ViewModelInject  constructor(
    private val shopRepository: ShopRepository
 ):ViewModel() {
-    val listOfEntries = listOf<SellEntry>()
+    var listOfEntries = listOf<SellEntry>()
+    val dateToday = ""
     val sellEntries = liveData {
         emit(shopRepository.getSellEntries())
     }
+    init {
+        initializeSellEntries()
+    }
 
+    val daysProfit = liveData {
+        emit(listOfEntries.filter { it.timeSold.contains(dateToday) }.sumOf { it.totalProfit })
+    }
 
-
-
+    private fun initializeSellEntries() {
+        viewModelScope.launch {
+            listOfEntries = shopRepository.getSellEntries()
+        }
+    }
 }
