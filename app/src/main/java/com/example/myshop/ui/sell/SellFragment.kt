4f85a9f1a09f70b3aab9b8f84dcf2848bfc5e19f
Kagiri11 <charlesmaish423@gmail.com>
2021-09-29 22:34:57 +0300

    Code reduction

--- app/src/main/java/com/example/myshop/ui/sell/SellFragment.kt
+++ app/src/main/java/com/example/myshop/ui/sell/SellFragment.kt
@@ -39,8 +39,7 @@ class SellFragment : Fragment() {
     private lateinit var binding: FragmentSellBinding
 
     @RequiresApi(Build.VERSION_CODES.O)
-    val timeSold: String =
-        LocalDateTime.now().format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT))!!
+    val timeSold: String = LocalDateTime.now().format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT))!!
 
 
     @SuppressLint("SetTextI18n")
