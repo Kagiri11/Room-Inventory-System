f7c2ba64c1470abf1f32db8d5d4caecae2b2dde3
Kagiri11 <charlesmaish423@gmail.com>
2021-09-30 00:51:49 +0300

    Added card to display daily profit

--- app/src/main/java/com/example/myshop/ui/sellhistory/SellHistoryViewModel.kt
+++ app/src/main/java/com/example/myshop/ui/sellhistory/SellHistoryViewModel.kt
@@ -4,12 +4,17 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
 import com.example.myshop.data.repositories.ShopRepository
+import com.example.myshop.model.SellEntry
 
 class SellHistoryViewModel @ViewModelInject  constructor(
    private val shopRepository: ShopRepository
 ):ViewModel() {
-
+    val listOfEntries = listOf<SellEntry>()
     val sellEntries = liveData {
         emit(shopRepository.getSellEntries())
     }
+
+
+
+
 }
